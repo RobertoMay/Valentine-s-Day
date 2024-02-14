2142fc66c81841d2c6d844dc543f14a1cce20477
RobertoMay <7630@itescam.edu.mx>
2024-02-14 14:13:40 -0600

    page2 is over

--- js/index.js
+++ js/index.js
@@ -1,8 +1,32 @@
 function move() {
     var no = document.getElementById("no");
     var container = document.getElementById("container");
-    var x = Math.floor(Math.random() * (container.offsetWidth - no.offsetWidth));
-    var y = Math.floor(Math.random() * (container.offsetHeight - no.offsetHeight));
+    var heart = document.querySelector(".heart");
+
+    var containerWidth = container.offsetWidth;
+    var containerHeight = container.offsetHeight;
+    var buttonWidth = no.offsetWidth;
+    var buttonHeight = no.offsetHeight;
+
+    var maxX = containerWidth - buttonWidth;
+    var maxY = containerHeight - buttonHeight;
+
+    var x = Math.floor(Math.random() * maxX);
+    var y = Math.floor(Math.random() * maxY);
+
+    var heartRect = heart.getBoundingClientRect();
+    var buttonRect = no.getBoundingClientRect();
+    
+    while (
+        x < heartRect.right && 
+        x + buttonWidth > heartRect.left &&
+        y < heartRect.bottom &&
+        y + buttonHeight > heartRect.top
+    ) {
+        x = Math.floor(Math.random() * maxX);
+        y = Math.floor(Math.random() * maxY);
+    }
+
     no.style.left = x + "px";
     no.style.top = y + "px";
 }
